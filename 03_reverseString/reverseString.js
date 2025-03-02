62f9a0403a007184ab66ca87ed04322307d8386f
J-Coulson <167429496+J-Coulson@users.noreply.github.com>
2025-03-02 22:48:04 +0000

    did exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    const reverseArray = [];
 
+    for(const character of input){
+        reverseArray.unshift(character);
+    }
+
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
