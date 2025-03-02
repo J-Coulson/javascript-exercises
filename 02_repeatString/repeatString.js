189f89ebec3dce728bee27d2ac5d5f936da50ab4
J-Coulson <167429496+J-Coulson@users.noreply.github.com>
2025-03-02 22:33:04 +0000

    did exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(rptString, numRepeats) {
+    let returnString = "";
+    
+    //negative repeats is meaningless
+    if(numRepeats < 0)
+    {
+        return "ERROR";
+    }
 
+    for(let i = 0; i < numRepeats; i++){
+        returnString += rptString;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
