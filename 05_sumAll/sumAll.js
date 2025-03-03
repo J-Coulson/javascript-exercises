657e5afaebef14a833027d864aa0bf75b7abe4ab
J-Coulson <167429496+J-Coulson@users.noreply.github.com>
2025-03-03 19:21:07 +0000

    did exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    //check error
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
+    if((a < 0) || (b < 0)){
+        return "ERROR";
+    }
+    if(typeof a !== "number" || typeof b !== "number"){
+        return "ERROR";
+    }
 
+    //determine order
+    let smaller = a<b ? a:b;
+    let greater = a<b ? b:a;
+
+    //sum
+    let total = 0;
+    for(let i = smaller; i <= greater; i++)
+    {
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
